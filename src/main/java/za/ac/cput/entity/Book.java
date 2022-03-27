8c62b23f0b2994b19a979431aa8dce411c4936d7
Ongezwa Gwaza <211272183@mycput.ac.za>
2022-03-27 16:13:42 +0200

    add book entity

---
+++ src/main/java/za/ac/cput/entity/Book.java
@@ -0,0 +1,102 @@
+/* Book.java
+Entity for the Book
+Author: Ongezwa Gwaza (211272183)
+Date: 27 March 2022
+*/
+package za.ac.cput.entity;
+
+public class Book {
+//    Attributes for the Book entity
+    private  String  bookCode;
+    private  String name;
+    private  String publisher;
+    private  String available;
+//   private final String supplierId;
+
+    /* Constructor for the book class
+    which uses BookBuilder to instantiate fields
+     */
+    private Book(BookBuilder bookBuilder) {
+        this.bookCode = bookBuilder.bookCode;
+        this.name = bookBuilder.name;
+        this.publisher = bookBuilder.publisher;
+        this.available = bookBuilder.available;
+    }
+
+//    Gettes and Setters for the Book attributes
+    public String getBookCode() {
+        return bookCode;
+    }
+
+    public void setBookCode(String bookCode) {
+        this.bookCode = bookCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(String available) {
+        this.available = available;
+    }
+
+//    toString method used to convert object attributes to  string data type
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookCode='" + bookCode + '\'' +
+                ", name='" + name + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", available='" + available + '\'' +
+                '}';
+    }
+
+//    Builder class used to implement the Builder patter
+    public static class BookBuilder{
+        private  String  bookCode;
+        private  String name;
+        private  String publisher;
+        private  String available;
+
+        public BookBuilder(String bookCode) {
+            this.bookCode = bookCode;
+        }
+
+        public BookBuilder nameOfBook(String name){
+            this.name = name;
+            return this;
+        }
+
+        public BookBuilder publishedBy(String publisher){
+            this.publisher = publisher;
+            return this;
+        }
+
+        public BookBuilder isBookAvailable(String available){
+            this.available = available;
+            return this;
+        }
+
+//        method for creating the instance of the book class
+        public Book build(){
+            Book book = new Book(this);
+            return  book;
+        }
+    }
+}
