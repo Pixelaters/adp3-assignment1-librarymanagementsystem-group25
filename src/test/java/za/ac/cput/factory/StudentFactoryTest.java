630467ddb8a4b902d9a94be497e3326f22f48b0b
breySean <breytonernstzen@gmail.com>
2022-03-29 17:59:35 +0200

    studentFactory class created and test cases done based on thise class

---
+++ src/test/java/za/ac/cput/factory/StudentFactoryTest.java
@@ -0,0 +1,107 @@
+package za.ac.cput.factory;
+/*
+    Breyton Ernstzen (217203027)
+    ADP3 - Assignment 1: Group 25
+    Date: 29 March 2022
+ */
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.entity.Student;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentFactoryTest {
+
+    @Test
+    public void addStudentTest1(){
+        Student newStudent = StudentFactory.createStudent(100,"Jane","Rice","Cat Ave",
+                321654987, 217203027,"Accounting",false,true);
+
+        System.out.println("Student added");
+        assertNotNull(newStudent);
+    }
+
+    @Test
+    public void addStudent2(){
+        //adds a new student, but with some missing values
+        Student newStudent = (Student) new Student.Builder()
+                .setStudentNumber(12345789)
+                .setUniversity(true)
+                .setCustomerID(2)
+                .setCustomerName("Ben")
+                .setCustomerSurname("Welsh")
+                .setAddress("Wonderland")
+                .build();
+
+        System.out.println("New student added!...");
+        assertNotNull(newStudent);
+       //assertNull(newStudent);//fail test
+
+        Student newStudent1 = null;
+        assertNull(newStudent1); //Object that is null
+
+    }
+
+    @Test
+    public void showStudents(){
+        Student newStudent = StudentFactory.createStudent(100,"Jane","Rice","Cat Ave",
+                321654987, 217203027,"Accounting",false,true);
+
+        System.out.println(newStudent);
+        assertNotNull(newStudent);//pass test
+        //assertNull(newStudent); //fail test
+    }
+
+    @Test
+    @Disabled
+    public void testObjectIdentity(){
+        //Test for object identity
+        Student student1 = StudentFactory.createStudent(100,"Jane","Rice","Cat Ave",
+                321654987, 217203027,"Accounting",false,true);
+        Student student2 = StudentFactory.createStudent(101,"Tim","Dixon","Lalaland",
+        246810121,219083479,"Marketing",false,true);
+        Student student3 = student1;
+
+        //assertSame(student1,student2); //failed test
+        assertSame(student1,student3);
+        assertSame(student3,student1);
+
+        //assertNotSame(student1,student3);//failed test
+        assertNotSame(student1,student2);
+        assertNotSame(student2,student1);
+        assertNotSame(student2,student3);
+        assertNotSame(student3,student2);
+        System.out.println("Test passed...");
+    }
+
+    @Test
+    public void testObjectEquality(){
+        Student student1 = StudentFactory.createStudent(100,"Jane","Rice","Cat Ave",
+                321654987, 217203027,"Accounting",false,true);
+        Student student2 = StudentFactory.createStudent(101,"Tim","Dixon","Lalaland",
+                246810121,219083479,"Marketing",false,true);
+        Student student3 = student1;
+        Student student4 = student2;
+
+        //assertEquals(student1,student2); //fail test
+        assertEquals(student1,student3);
+        assertEquals(student3,student1);
+        assertEquals(student4,student2);
+        assertEquals(student2,student4);
+
+        assertEquals(true,student1.isUniversity());
+        //assertEquals(true,student1.isHighSchool()); //failed test
+        assertEquals(false,student1.isHighSchool());
+        //assertEquals(false,student1.isUniversity()); //failed test
+
+        assertEquals(true,student2.isUniversity());
+        //assertEquals(true,student2.isHighSchool()); //failed test
+        assertEquals(false,student2.isHighSchool());
+        //assertEquals(false,student2.isUniversity()); //failed test
+
+        System.out.println("All tests passed...");
+    }
+
+}
