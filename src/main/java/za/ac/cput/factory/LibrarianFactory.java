e3758de34acee6fe24fda3468b3429c93bd8666a
Ziyaad <Zpetersen@Bainscorp.com>
2022-03-29 23:28:45 +0200

    added comments for my name date, assignment and java file

--- src/main/java/za/ac/cput/factory/LibrarianFactory.java
+++ src/main/java/za/ac/cput/factory/LibrarianFactory.java
@@ -2,6 +2,13 @@ package za.ac.cput.factory;
 
 import za.ac.cput.entity.Librarian;
 
+/*
+    LibrarianFactory.java
+    Ziyaad Petersen (219083479)
+    ADP3 - Assignment 1: Group 25
+    Date: 29 March 2022
+ */
+
 public class LibrarianFactory {
     public static Librarian createLibrarian(long staffID, String firstName, String lastName, String homeAddress, long cellPhone, String librarianCode, String position, boolean Availability){
         Librarian newLibrarian = (Librarian) new Librarian
