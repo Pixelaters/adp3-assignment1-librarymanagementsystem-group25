5ff8e2903afbf0041ef63a4ef06512161a040a67
breySean <breytonernstzen@gmail.com>
2022-03-29 17:59:35 +0200

    studentFactory class created and test cases done based on thise class

--- src/main/java/za/ac/cput/factory/StudentFactory.java
+++ src/main/java/za/ac/cput/factory/StudentFactory.java
@@ -1,5 +1,24 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.entity.Student;
+
 public class StudentFactory {
 
+    public static Student createStudent(int id,String name,String surname,String address,long cellNo,
+                                     long studentNo,String course,boolean high_school,boolean university){
+
+        Student newStudent = (Student) new Student.Builder()
+                .setStudentNumber(studentNo)
+                .setCourse(course)
+                .setHighSchool(high_school)
+                .setUniversity(university)
+                .setCustomerID(id)
+                .setCustomerName(name)
+                .setCustomerSurname(surname)
+                .setAddress(address)
+                .setCellNo(cellNo)
+                .build();
+
+        return newStudent;
+    }
 }
