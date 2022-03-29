c0e97b3ab58c8929989378f0692c5774708d46e6
breySean <breytonernstzen@gmail.com>
2022-03-29 18:19:05 +0200

    adult object/structure created using builder pattern

--- src/main/java/za/ac/cput/factory/AdultFactory.java
+++ src/main/java/za/ac/cput/factory/AdultFactory.java
@@ -1,8 +1,28 @@
 package za.ac.cput.factory;
+
+import za.ac.cput.entity.Adult;
+
 /*
     Breyton Ernstzen (217203027)
     ADP3 - Assignment 1: Group 25
     Date: 29 March 2022
  */
 public class AdultFactory {
+
+    public static Adult createAdult(int id,String name,String surname,String address,long cellNo,
+                                    boolean pensioner,boolean employed,String jobDescription){
+
+        Adult newAdult = (Adult) new Adult.Builder()
+                .setPensioner(pensioner)
+                .setEmployed(employed)
+                .setJobDescription(jobDescription)
+                .setCustomerID(id)
+                .setCustomerName(name)
+                .setCustomerSurname(surname)
+                .setAddress(address)
+                .setCellNo(cellNo)
+                .build();
+
+            return newAdult;
+    }
 }
