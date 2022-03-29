2ff26bc80bc32af98453c157aa110f7152dd4a54
Ziyaad <Zpetersen@Bainscorp.com>
2022-03-29 23:28:45 +0200

    added comments for my name date, assignment and java file

--- src/main/java/za/ac/cput/factory/ManagerFactory.java
+++ src/main/java/za/ac/cput/factory/ManagerFactory.java
@@ -2,6 +2,13 @@ package za.ac.cput.factory;
 
 import za.ac.cput.entity.Manager;
 
+/*
+    ManagerFactory.java
+    Ziyaad Petersen (219083479)
+    ADP3 - Assignment 1: Group 25
+    Date: 29 March 2022
+ */
+
 public class ManagerFactory {
     public static Manager createManager(long staffID, String firstName, String lastName, String homeAddress, long cellPhone, String managerCode, boolean Availability){
     Manager newManager = (Manager) new Manager
