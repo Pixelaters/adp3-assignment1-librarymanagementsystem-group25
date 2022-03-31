7a541039c07a9febf96e99f3947da6e03b8fcb3a
Raeece <raeecesamuels100@gmail.com>
2022-03-31 18:50:19 +0200

    Create Transaction.java

---
+++ src/main/java/za/ac/cput/entity/Transaction.java
@@ -0,0 +1,86 @@
+package za.ac.cput.entity;
+/**
+ * Transaction.java
+ * Raeece Samuels (217283764)
+ * ADP3 - Assignment1: Group 25
+ * Date: 27 March 2022
+ */
+
+
+
+public class Transaction {
+    private String transaction_ID;
+    private String date;
+    private String details;
+
+    private Transaction(TransactionBuilder tb){
+        this.transaction_ID = tb.transaction_ID;
+        this.date = tb.date;
+        this.details = tb.details;
+    }
+
+    public String getTransaction_ID() {
+        return transaction_ID;
+    }
+
+    public void setTransaction_ID(String transaction_ID) {
+        this.transaction_ID = transaction_ID;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "transaction_ID='" + transaction_ID + '\'' +
+                ", date=" + date +
+                ", details='" + details + '\'' +
+                '}';
+    }
+    public static class TransactionBuilder{
+        private String transaction_ID;
+        private String date;
+        private String details;
+
+        public TransactionBuilder setTransaction_ID(String transaction_ID) {
+            this.transaction_ID = transaction_ID;
+            return this;
+        }
+
+        public TransactionBuilder setDate(String date) {
+            this.date = date;
+            return this;
+        }
+
+        public TransactionBuilder setDetails(String details) {
+            this.details = details;
+            return this;
+        }
+
+        public TransactionBuilder copy(Transaction t){
+            this.transaction_ID = t.transaction_ID;
+            this.date = t.date;
+            this.details = t.details;
+            return this;
+        }
+        public Transaction build(){
+            return new Transaction(this);
+        }
+    }
+
+
+}
