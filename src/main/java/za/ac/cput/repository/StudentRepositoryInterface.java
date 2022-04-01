271d68c22898f7a5cce7384ef5ce382607488f25
breySean <breytonernstzen@gmail.com>
2022-04-01 16:36:47 +0200

    StudentRepositoryInterface class created with a get all method

---
+++ src/main/java/za/ac/cput/repository/StudentRepositoryInterface.java
@@ -0,0 +1,14 @@
+package za.ac.cput.repository;
+/*
+    Breyton Ernstzen (217203027)
+    ADP3 - Assignment 1: Group 25
+    Date: 1 April 2022
+ */
+import za.ac.cput.entity.Student;
+
+import java.util.Set;
+
+public interface StudentRepositoryInterface {
+
+    public Set<Student> getAll();
+}
