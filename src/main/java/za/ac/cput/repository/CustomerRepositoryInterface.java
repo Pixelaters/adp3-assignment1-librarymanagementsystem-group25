71e13ee991d08df6f72b4a3c89e97eb2519f7c05
breySean <breytonernstzen@gmail.com>
2022-04-01 12:28:53 +0200

    CustomerRepositoryInterface class created

---
+++ src/main/java/za/ac/cput/repository/CustomerRepositoryInterface.java
@@ -0,0 +1,8 @@
+package za.ac.cput.repository;
+/*
+    Breyton Ernstzen (217203027)
+    ADP3 - Assignment 1: Group 25
+    Date: 1 April 2022
+ */
+public interface CustomerRepositoryInterface {
+}
