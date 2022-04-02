d531583159a81f0fef6c5bf2beea96a733282cb4
breySean <breytonernstzen@gmail.com>
2022-04-02 13:19:10 +0200

    create and read method tested

---
+++ src/test/java/za/ac/cput/repository/AdultRepositoryTest.java
@@ -0,0 +1,71 @@
+package za.ac.cput.repository;
+/*
+    Breyton Ernstzen (217203027)
+    ADP3 - Assignment 1: Group 25
+    Date: 2 April 2022
+ */
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import za.ac.cput.entity.Adult;
+import za.ac.cput.factory.AdultFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)
+class AdultRepositoryTest {
+    private static AdultRepository adultRepository = AdultRepository.getInstance();
+    private static Adult adult_a = AdultFactory.createAdult(1234,"Atticus","Smith",
+            "Court street",2022040212,false,true,"Lawyer");
+    private static Adult adult_b = AdultFactory.createAdult(1212,"Roy","Hodgson",
+            "Eagle ave",124520202,true,false,"retired");
+
+    //Test to add an adult customer to the LMS
+    @Test
+    void a_CreateAdult(){
+        Adult newAdult1 = adultRepository.create(adult_a);
+        Adult newAdult2 = adultRepository.create(adult_b);
+        Adult newAdult3 = newAdult1;
+        Adult newAdult4 = newAdult2;
+
+        assertNotNull(newAdult1);
+        assertNotNull(newAdult2);
+        assertEquals(newAdult1,newAdult3);
+        assertEquals(newAdult2,newAdult4);
+        assertNotSame(adult_a,adult_b);
+        assertSame(adult_a,newAdult1);
+        assertSame(adult_b,newAdult2);
+        assertSame(adult_a.isEmployed(),newAdult1.isEmployed());
+        assertEquals(adult_a.isPensioner(),newAdult1.isPensioner());
+        //assertSame(adult_a,adult_b); //fail test
+        System.out.println("Adult customer added...");
+        System.out.println();
+    }
+
+    //Read a specific adult customer from the database
+    @Test
+    void b_ReadAdult(){
+       Adult readAdult = adultRepository.read(adult_a.getCustomerID());
+       Adult anotherAdult = readAdult;
+
+       assertNotNull(readAdult);
+       assertSame(adult_a,readAdult);
+       assertSame(adult_a.isEmployed(),readAdult.isEmployed());
+       assertEquals(adult_a.isPensioner(),readAdult.isPensioner());
+       assertNotSame(adult_b,readAdult);
+       assertEquals(adult_a,readAdult);
+       assertEquals(anotherAdult,readAdult);
+       //assertSame(adult_a,adult_b);//fails test
+
+        System.out.println("Reading adult customer...");
+        System.out.println(adultRepository.read(1234));
+        System.out.println();
+    }
+
+    //test to update a specific adult customer details
+    @Test
+    void c_UpdateAdult(){
+
+    }
+
+
+}
