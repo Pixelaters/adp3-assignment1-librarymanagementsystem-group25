3c014bc9e9a5b8ab191e6b147c3151f97a2bfd16
breySean <breytonernstzen@gmail.com>
2022-04-03 19:42:18 +0200

    test methods added

--- src/test/java/za/ac/cput/factory/StudentFactoryTest.java
+++ src/test/java/za/ac/cput/factory/StudentFactoryTest.java
@@ -66,12 +66,20 @@ class StudentFactoryTest {
 
         //assertSame(student1,student2); //failed test
         assertSame(student1,student3);
+        assertSame(student1.getCustomerID(),student3.getCustomerID());
+        assertSame(student1.getCustomerName(),student3.getCustomerName());
+        assertSame(student1.getCustomerSurname(),student3.getCustomerSurname());
+        assertSame(student1.getAddress(),student3.getAddress());
         assertSame(student3,student1);
         assertSame(student1.isHighSchool(),student2.isHighSchool());
         assertSame(student1.isUniversity(),student2.isUniversity());
 
         //assertNotSame(student1,student3);//failed test
         assertNotSame(student1.getCustomerID(),student2.getCustomerID());
+        assertNotSame(student1.getCustomerName(),student2.getCustomerName());
+        assertNotSame(student1.getCustomerSurname(),student2.getCustomerSurname());
+        assertNotSame(student1.getAddress(),student2.getAddress());
+        assertNotSame(student1.getCellNo(),student2.getCellNo());
         assertNotSame(student1.getStudentNumber(),student2.getStudentNumber());
         assertNotSame(student1,student2);
         assertNotSame(student2,student1);
