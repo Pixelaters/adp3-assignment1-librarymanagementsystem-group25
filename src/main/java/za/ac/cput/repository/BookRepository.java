852945ff86070a37270d8d85c5636dece3472593
Ongezwa Gwaza <211272183@mycput.ac.za>
2022-04-05 10:44:57 +0200

    add implementation for repository and repositoryInterface

--- src/main/java/za/ac/cput/repository/BookRepository.java
+++ src/main/java/za/ac/cput/repository/BookRepository.java
@@ -1,4 +1,135 @@
 package za.ac.cput.repository;
 
-public class BookRepository {
+/* BookRepository.java
+Repository interface for the Book
+Author: Ongezwa Gwaza (211272183)
+Date: 4 April 2022
+*/
+
+import za.ac.cput.entity.Book;
+import za.ac.cput.entity.Supplier;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class BookRepository implements BookRepositoryInterface{
+    //initialise variables and database
+    private static BookRepository bookRepository = null;
+    private Set<Book> booksDB = null;
+
+    private BookRepository(){
+        booksDB = new HashSet<Book>();
+    }
+
+    /*
+    Check if a repository instance is already created.
+    Create one if non is created
+    Uses the singleton pattern
+    */
+    public static BookRepository getInstance(){
+
+        if(bookRepository == null){
+            bookRepository = new BookRepository();
+        }
+
+        return bookRepository;
+    }
+
+    //adds a new book to the database
+    @Override
+    public Book create(Book book) {
+        boolean bookAdded = booksDB.add(book);
+//        if bookAdded is added return the book otherwise return null
+        return bookAdded ? book : null;
+    }
+
+    //Gets a book from the Database using the bookCode
+    @Override
+    public Book read(String bookCode) {
+
+        //iterate  through the book list and return the book whose bookCode equals the input
+        for(Book book: booksDB){
+            if(book.getBookCode().equals(bookCode))
+                return book;
+        }
+        //book not found
+        return null;
+    }
+
+
+    //Update a book in the Database
+    @Override
+    public Book update(Book book) {
+        //Start by getting the book from the database
+        Book oldBook = read(book.getBookCode());
+
+        if(oldBook != null) {
+            //if the book is in the database
+            //remove old book
+            //add the new book
+            booksDB.remove(oldBook);
+            booksDB.add(book);
+            return book; //return the updated book
+        }
+        return null;
+    }
+
+    //Delete book by bookCode
+    @Override
+    public boolean delete(String bookCode) {
+        //Start by getting the book from the database using the bookCode
+        Book bookToDelete = read(bookCode);
+
+        //if book is not available in database return false;
+        if(bookToDelete == null)
+            return false;
+        /*
+        if book is available remove/delete it
+        if book was present and has been removed
+        the  remove() method will return true otherwise it will return false
+        */
+        return booksDB.remove(bookToDelete);
+    }
+
+    //list all the books in the Database
+    @Override
+    public Set<Book> getAll() {
+        return booksDB;
+    }
+
+    //Gets a book from the Database using the name of the book
+    @Override
+    public Book getBookByName(String name) {
+        //iterate  through the book list and return the book with the given name
+        for(Book book: booksDB){
+            if(book.getName().equals(name))
+                return book;
+        }
+        //book not found
+        return null;
+    }
+
+    //Gets a book from the Database using the publisher of the book
+    @Override
+    public Book getBookByPublisher(String publisher) {
+        //iterate  through the book list and return the book with the given publisher
+        for(Book book: booksDB){
+            if(book.getPublisher().equals(publisher))
+                return book;
+        }
+        //book not found
+        return null;
+    }
+
+    //Gets a book from the Database using the supplier of the book
+    @Override
+    public Book getBookBySupplier(Supplier supplier) {
+        //iterate  through the book list and return the book with the given supplier
+        for(Book book: booksDB){
+            if(book.getSupplierId().equals(supplier))
+                return book;
+        }
+        //book not found
+        return null;
+    }
 }
--- src/main/java/za/ac/cput/repository/SupplierRepository.java
+++ src/main/java/za/ac/cput/repository/SupplierRepository.java
@@ -1,2 +1,135 @@
-package za.ac.cput.repository;public class SupplierRepository {
+package za.ac.cput.repository;
+/* SupplierRepository.java
+Repository class for the Supplier
+Author: Ongezwa Gwaza (211272183)
+Date: 4 April 2022
+*/
+
+import za.ac.cput.entity.Book;
+import za.ac.cput.entity.Library;
+import za.ac.cput.entity.Supplier;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SupplierRepository implements SupplierRepositoryInterface{
+    //initialise variables and database
+    private static SupplierRepository supplierRepository = null;
+    private Set<Supplier> suppliersDB = null;
+
+    private SupplierRepository(){
+        suppliersDB = new HashSet<Supplier>();
+    }
+
+    /*
+    Check if a repository instance is already created.
+    Create one if non is created
+    Uses the singleton pattern
+    */
+    public static SupplierRepository getInstance(){
+
+        if(supplierRepository == null){
+            supplierRepository = new SupplierRepository();
+        }
+
+        return supplierRepository;
+    }
+
+    //adds a new supplier to the database
+    @Override
+    public Supplier create(Supplier supplier) {
+        boolean supplierAdded = suppliersDB.add(supplier);
+//        if supplierAdded is added return the supplier otherwise return null
+        return supplierAdded ? supplier : null;
+    }
+
+    //Gets a supplier from the Database using the supplierId
+    @Override
+    public Supplier read(String supplierId) {
+
+        //iterate  through the supplier list and return the supplier whose supplierId equals the input
+        for(Supplier supplier: suppliersDB){
+            if(supplier.getSupplierId().equals(supplierId))
+                return supplier;
+        }
+        //supplier not found
+        return null;
+    }
+
+
+    //Update a supplier in the Database
+    @Override
+    public Supplier update(Supplier supplier) {
+        //Start by getting the supplier from the database
+        Supplier oldSupplier = read(supplier.getSupplierId());
+
+        if(oldSupplier != null) {
+            //if the supplier is in the database
+            //remove old supplier
+            //add the new supplier
+            suppliersDB.remove(oldSupplier);
+            suppliersDB.add(supplier);
+            return supplier; //return the updated supplier
+        }
+        return null;
+    }
+
+    //Delete supplier by supplierId
+    @Override
+    public boolean delete(String supplierId) {
+        //Start by getting the supplier from the database using the supplierId
+        Supplier supplierToDelete = read(supplierId);
+
+        //if supplier is not available in database return false;
+        if(supplierToDelete == null)
+            return false;
+        /*
+        if supplier is available remove/delete it
+        if supplier was present and has been removed
+        the  remove() method will return true otherwise it will return false
+        */
+        return suppliersDB.remove(supplierToDelete);
+    }
+
+    //list all the suppliers in the Database
+    @Override
+    public Set<Supplier> getAll() {
+        return suppliersDB;
+    }
+
+    //Gets a supplier from the Database using the name of the supplier
+    @Override
+    public Supplier getSupplierByName(String name) {
+        //iterate  through the supplier list and return the supplier with the given name
+        for(Supplier supplier: suppliersDB){
+            if(supplier.getName().equals(name))
+                return supplier;
+        }
+        //supplier not found
+        return null;
+    }
+
+    //Gets a supplier from the Database using the publisher of the city
+    @Override
+    public Supplier getSupplierByCity(String city) {
+        //iterate  through the supplier list and return the supplier with the given city
+        for(Supplier supplier: suppliersDB){
+            if(supplier.getCity().equals(city))
+                return supplier;
+        }
+        //supplier not found
+        return null;
+    }
+
+    //Gets a supplier from the Database using the library
+    @Override
+    public Supplier getSupplierByLibrary(Library library) {
+        //iterate  through the supplier list and return the supplier with the given library
+        for(Supplier supplier: suppliersDB){
+            if(supplier.getLibraryId().equals(library))
+                return supplier;
+        }
+        //supplier not found
+        return null;
+    }
 }
