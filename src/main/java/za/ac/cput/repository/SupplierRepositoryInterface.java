c9830937f2817259eb9c214e6ed9d2ee7387b9c0
Ongezwa Gwaza <211272183@mycput.ac.za>
2022-04-05 10:44:57 +0200

    add implementation for repository and repositoryInterface

--- src/main/java/za/ac/cput/repository/SupplierRepositoryInterface.java
+++ src/main/java/za/ac/cput/repository/SupplierRepositoryInterface.java
@@ -1,2 +1,18 @@
-package za.ac.cput.repository;public class SupplierRepositoryInterface {
+package za.ac.cput.repository;
+
+import za.ac.cput.entity.Supplier;
+import za.ac.cput.entity.Library;
+
+import java.util.Set;
+
+/* SupplierRepositoryInterface.java
+Repository interface for the Supplier
+Author: Ongezwa Gwaza (211272183)
+Date: 4 April 2022
+*/
+public interface SupplierRepositoryInterface extends IRepository<Supplier,String>{
+    Set<Supplier> getAll();
+    Supplier getSupplierByName(String name);
+    Supplier getSupplierByCity(String publisher);
+    Supplier getSupplierByLibrary(Library library);
 }
