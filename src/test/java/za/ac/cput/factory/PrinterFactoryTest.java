4cd65c9e798ceac593fd03ad2df435a62bedc110
nondumiso166 <220430853@mycput.ac.za>
2022-04-08 20:36:18 +0200

    Addded Printer and CustomerPrinter repository and their test.

--- src/test/java/za/ac/cput/factory/PrinterFactoryTest.java
+++ src/test/java/za/ac/cput/factory/PrinterFactoryTest.java
@@ -1,3 +1,11 @@
+/**
+
+ * This is a PrinterFactoryTest.java class .
+ *Nondumiso Gaga (2204308543)
+ * ADP Assignment1 term1 Group25
+ * date:30/03/2022
+ */
+
 package za.ac.cput.factory;
 
 import org.junit.jupiter.api.Test;
