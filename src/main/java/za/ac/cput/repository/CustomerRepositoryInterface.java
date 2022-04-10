1ada9dedb0434ff5d3d8f1d6b0f15520175460d3
breySean <breytonernstzen@gmail.com>
2022-04-10 10:41:33 +0200

    my student details added in comment section

--- src/main/java/za/ac/cput/repository/CustomerRepositoryInterface.java
+++ src/main/java/za/ac/cput/repository/CustomerRepositoryInterface.java
@@ -1,14 +1,13 @@
 package za.ac.cput.repository;
-
-import za.ac.cput.entity.Customer;
-
-import java.util.Set;
-
 /*
     Breyton Ernstzen (217203027)
     ADP3 - Assignment 1: Group 25
     Date: 1 April 2022
  */
+import za.ac.cput.entity.Customer;
+
+import java.util.Set;
+
 public interface CustomerRepositoryInterface extends IRepository<Customer, Integer>{
 
     public Set<Customer> getAll();
