37fdbad586ed77c7c0058cf9fe2b54c8b4e18092
Raeece <raeecesamuels100@gmail.com>
2022-08-06 16:08:19 +0200

    Did my part also did Librarian i needed the info

---
+++ src/main/java/za/ac/cput/domain/Address.java
@@ -0,0 +1,106 @@
+package za.ac.cput.domain;
+/*
+* Name: Raeece Samuels
+* Project: Library Management
+* Entity: Address class
+* Date: 2022/08/06
+* */
+
+
+public class Address {
+
+    private String unitNumber, complexName, streetNumber, streetName;
+    private int postalCode;
+    private City city;
+
+    public Address(AddressBuilder ab){
+        this.unitNumber = ab.unitNumber;
+        this.complexName = ab.complexName;
+        this.streetNumber = ab.streetNumber;
+        this.streetName = ab.streetName;
+        this.postalCode = ab.postalCode;
+        this.city = ab.city;
+
+
+    }
+
+    public String getUnitNumber() {
+        return unitNumber;
+    }
+
+    public String getComplexName() {
+        return complexName;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "unitNumber='" + unitNumber + '\'' +
+                ", complexName='" + complexName + '\'' +
+                ", streetNumber='" + streetNumber + '\'' +
+                ", streetName='" + streetName + '\'' +
+                ", postalCode=" + postalCode +
+                '}';
+    }
+
+    public static class AddressBuilder {
+        private String unitNumber, complexName, streetNumber, streetName;
+        private int postalCode;
+        private City city;
+
+        public AddressBuilder unitNumber(String unitNumber) {
+            this.unitNumber = unitNumber;
+            return this;
+        }
+
+        public AddressBuilder complexName(String complexName) {
+            this.complexName = complexName;
+            return this;
+        }
+
+        public AddressBuilder streetNumber(String streetNumber) {
+            this.streetNumber = streetNumber;
+            return this;
+        }
+
+        public AddressBuilder streetName(String streetName) {
+            this.streetName = streetName;
+            return this;
+        }
+
+        public AddressBuilder postalCode(int postalCode) {
+            this.postalCode = postalCode;
+            return this;
+        }
+        public AddressBuilder City(City city){
+            this.city = city;
+            return this;
+        }
+
+       public AddressBuilder copy(Address a){
+            this.unitNumber = a.unitNumber;
+            this.complexName = a.complexName;
+            this.streetNumber = a.streetNumber;
+            this.streetName = a.streetName;
+            this.postalCode = a.postalCode;
+            this.city = a.city;
+            return this;
+       }
+    }
+}
---
+++ src/main/java/za/ac/cput/domain/Librarian.java
@@ -0,0 +1,69 @@
+package za.ac.cput.domain;
+/*
+ * Name: Raeece Samuels
+ * Project: Library Management
+ * Entity: Librarian class (Didn't have to do this)
+ * Date: 2022/08/06
+ * */
+public class Librarian {
+    private String staffId, position;
+    private Name name;
+
+    private Librarian(LibrarianBuilder lb) {
+        this.staffId = lb.staffId;
+        this.position = lb.position;
+        this.name = lb.name;
+    }
+
+    public String getStaffId() {
+        return staffId;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Librarian{" +
+                "staffId='" + staffId + '\'' +
+                ", name='" + name + '\'' +
+                ", position=" + position+
+                '}';
+    }
+
+    public static class LibrarianBuilder{
+        private String staffId, position;
+        private Name name;
+
+        public LibrarianBuilder staffId(String staffId) {
+            this.staffId = staffId;
+            return this;
+        }
+
+        public LibrarianBuilder position(String position) {
+            this.position = position;
+            return this;
+        }
+
+        public LibrarianBuilder name(Name name) {
+            this.name = name;
+            return this;
+        }
+
+        public LibrarianBuilder copy(Librarian l){
+            this.staffId = l.staffId;
+            this.name = l.name;
+            this.position = l.position;
+            return this;
+        }
+
+        public Librarian build(){
+            return new Librarian(this);
+        }
+    }
+}
