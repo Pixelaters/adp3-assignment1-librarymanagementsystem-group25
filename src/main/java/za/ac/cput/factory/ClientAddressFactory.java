4a3aa40519d2d309736ca9e8eb4d1da6a367b05a
breySean <breytonernstzen@gmail.com>
2022-08-06 18:45:07 +0200

    pom file fixed

---
+++ src/main/java/za/ac/cput/factory/ClientAddressFactory.java
@@ -0,0 +1,21 @@
+/*
+    Breyton Ernstzen (217203027)
+    Applications Development
+ */
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Address;
+import za.ac.cput.domain.ClientAddress;
+
+public class ClientAddressFactory {
+
+    public static ClientAddress builder(String clientId, Address address){
+
+        if(clientId.isEmpty())
+            throw new IllegalArgumentException("Id cannot be null");
+        return new ClientAddress.Builder()
+                .clientId(clientId)
+                .address(address)
+                .build();
+    }
+}
