051934f603d5a8874dffe64fd9b80f9fa1b0a52f
breySean <breytonernstzen@gmail.com>
2022-08-06 22:17:24 +0200

    database connectivity set

--- src/test/java/za/ac/cput/factory/ClientAddressFactoryTest.java
+++ src/test/java/za/ac/cput/factory/ClientAddressFactoryTest.java
@@ -7,13 +7,44 @@ import za.ac.cput.domain.ClientAddress;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClientAddressFactoryTest {
+    private ClientAddress ca1,ca2;
 
     @BeforeEach
     void setUp() {
+        ca1 = ClientAddressFactory.createClientAddress("1", AddressFactory.build("123",
+                "McTominay Estate","123","Kruger Ave",7798,
+                CityFactory.build("A12","Cape Town","Wynberg")));
+
+        ca2 = ClientAddressFactory.createClientAddress("2", AddressFactory.build("123",
+                "Bergkruine","123","Voortrekker rd",7798,
+                CityFactory.build("CT12","Cape Town","Vanguard")));
+
     }
 
     @Test
-    void test1(){
-        //ClientAddress ca = ClientAddressFactory.createClientAddress("1",)
+    void objectIdentity(){
+
+        assertAll(
+                () -> assertNotSame(ca1.getClientId(),ca2.getClientId()),
+                () -> assertSame("1", ca1.getClientId()),
+                () -> assertSame("2",ca2.getClientId()),
+                () -> assertNotNull(ca1.getClientId()),
+                () -> assertNotNull(ca2.getClientId())
+        );
+        System.out.println("Test passed");
+
+    }
+
+    @Test
+    void objectEquality(){
+
+        assertAll(
+                () -> assertNotEquals(ca1.getClientId(),ca2.getClientId()),
+                () -> assertNotEquals(ca2,ca1),
+                () -> assertNotEquals(ca1.getAddress(),ca2.getAddress())
+        );
+
+        System.out.println("Test passed");
+
     }
 }
