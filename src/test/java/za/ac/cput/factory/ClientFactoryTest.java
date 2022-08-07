2cb3694431378f39c1afeebe38b5319e3ed63e4d
breySean <breytonernstzen@gmail.com>
2022-08-07 13:38:26 +0200

    test cases completed for ClientFactory

--- src/test/java/za/ac/cput/factory/ClientFactoryTest.java
+++ src/test/java/za/ac/cput/factory/ClientFactoryTest.java
@@ -5,6 +5,7 @@
 package za.ac.cput.factory;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import za.ac.cput.domain.Client;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,6 +22,55 @@ class ClientFactoryTest {
         client2 = ClientFactory.createClient("2",NameFactory.createName("Ziyaad","Petersen"),
                 BookFactory.CreateBook("B141","My Neighbour Totoro","Kim Jeen","Fiction"),
                 true);
+    }
+
+    @Test
+    void clientWithMiddleName(){
+
+        assertAll(
+                () -> assertNotNull(client1.getName().getMiddleName()),
+                () -> assertSame("Sean",client1.getName().getMiddleName()),
+                () -> assertNotNull(client1)
+
+        );
+
+        System.out.println("Test successfully passed");
+        System.out.println(client1.toString());
+    }
+
+    @Test
+    void clientWithoutMiddleName(){
+
+        assertAll(
+                () -> assertNull(client2.getName().getMiddleName()),
+                () -> assertNotSame("",client2.getName().getMiddleName())
+
+        );
+        System.out.println("Test successfully passed");
+        System.out.println(client2.toString());
+    }
+
+    @Test
+    void objectEquality(){
+
+        assertAll(
+                () -> assertNotEquals(client1.getId(),client2.getId()),
+                () -> assertNotEquals(client1.getName().getMiddleName(),client2.getName().getMiddleName()),
+                () -> assertEquals(client1.isRented(),client2.isRented())
+        );
+
+        System.out.println("Test successfully passed");
+    }
+
+    @Test
+    void objectIdentity(){
+
+        assertAll(
+                () -> assertNotSame(client1.getId(),client2.getId()),
+                () -> assertNotSame(client1.getName().getMiddleName(),client2.getName().getMiddleName()),
+                () -> assertSame(client1.isRented(),client2.isRented())
+
+        );
     }
 
 }
