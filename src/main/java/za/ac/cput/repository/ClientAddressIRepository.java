ba8bfa54e2cf182ee42033253d5a24b47cb48a4b
breySean <breytonernstzen@gmail.com>
2022-08-08 10:58:48 +0200

    functionality added

--- src/main/java/za/ac/cput/repository/ClientAddressIRepository.java
+++ src/main/java/za/ac/cput/repository/ClientAddressIRepository.java
@@ -2,9 +2,11 @@ package za.ac.cput.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.Client;
 import za.ac.cput.domain.ClientAddress;
 
 @Repository
 public interface ClientAddressIRepository extends JpaRepository<ClientAddress,String> {
 
+    public ClientAddressIRepository findClientAddressByClientId(String clientId);
 }
