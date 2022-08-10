56485fa7b140038701f63c4eccd787edb5ccfeec
breySean <breytonernstzen@gmail.com>
2022-08-10 21:16:47 +0200

    test cases added for ClientService

---
+++ src/test/java/za/ac/cput/service/impl/ClientAddressServiceTest.java
@@ -0,0 +1,33 @@
+package za.ac.cput.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientAddressServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
