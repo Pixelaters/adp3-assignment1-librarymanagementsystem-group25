9988527b8c4781b8ef36b37d23a0165f69adb329
Raeece <raeecesamuels100@gmail.com>
2022-08-11 14:47:47 +0200

    Services completed and added annotations for address and Librarian

---
+++ src/main/java/za/ac/cput/service/impl/LibrarianAddressIService.java
@@ -0,0 +1,9 @@
+package za.ac.cput.service.impl;
+
+import za.ac.cput.domain.Address;
+import za.ac.cput.domain.LibrarianAddress;
+import za.ac.cput.service.IService;
+
+public interface LibrarianAddressIService extends IService<LibrarianAddress, String> {
+
+}
