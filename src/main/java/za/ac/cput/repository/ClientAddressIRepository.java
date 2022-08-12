413afc8c78905845c8912e9e71ac967ee9930c2d
breySean <breytonernstzen@gmail.com>
2022-08-12 09:48:13 +0200

    comments added for ClientIRepository and ClientAddressIRepository-classes

--- src/main/java/za/ac/cput/repository/ClientAddressIRepository.java
+++ src/main/java/za/ac/cput/repository/ClientAddressIRepository.java
@@ -1,5 +1,9 @@
 package za.ac.cput.repository;
-
+/*
+    Breyton Ernstzen (217203027)
+    ADP3
+    Capstone project
+ */
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Client;
