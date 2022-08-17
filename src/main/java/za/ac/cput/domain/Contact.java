9eaa2ac5c47a0a1d195edeba5f872814a33bb33e
Ziyaad <Zpetersen@Bainscorp.com>
2022-08-17 23:36:19 +0200

    added clientContact code

--- src/main/java/za/ac/cput/domain/Contact.java
+++ src/main/java/za/ac/cput/domain/Contact.java
@@ -1,17 +1,31 @@
 package za.ac.cput.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.Set;
 
 @Entity
 public class Contact implements Serializable {
     @Id
+    @Column(name= "contactId")
     private String contactId;
 
+    @NotNull
     private String email;
+
+    @NotNull
     private String cell;
+
+    @NotNull
     private String nextOfKin;
+
+    @OneToMany(mappedBy = "contact")
+    private Set<ClientContact> clientContactSet;
+
 
     protected Contact() {
 
