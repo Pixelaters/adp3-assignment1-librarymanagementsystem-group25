03525bd77f0df4d020fef6e7cd60c646694c993d
breySean <breytonernstzen@gmail.com>
2022-08-17 21:30:01 +0200

    comment

--- src/main/java/za/ac/cput/service/impl/ClientBookImpl.java
+++ src/main/java/za/ac/cput/service/impl/ClientBookImpl.java
@@ -1,5 +1,5 @@
 package za.ac.cput.service.impl;
-
+//
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import za.ac.cput.domain.ClientBook;
