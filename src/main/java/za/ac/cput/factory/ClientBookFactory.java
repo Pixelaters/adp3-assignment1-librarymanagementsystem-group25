a68fb6ddae962a71a05824120c6a273697715ae4
Ziyaad <Zpetersen@Bainscorp.com>
2022-08-23 18:22:41 +0200

    wokring client id without working proper working controller

--- src/main/java/za/ac/cput/factory/ClientBookFactory.java
+++ src/main/java/za/ac/cput/factory/ClientBookFactory.java
@@ -1,15 +1,15 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.Book;
+import za.ac.cput.domain.Client;
 import za.ac.cput.domain.ClientBook;
 
 public class ClientBookFactory {
-    public static ClientBook createClientBook(String clientId,String bookId){
-        if(clientId.isEmpty()||bookId.isEmpty())
-            throw new IllegalArgumentException("Id's cannot be left empty");
-
-            return new ClientBook.Builder()
-                    .clientId(clientId)
-                    .bookId(bookId)
-                    .build();
+    public static ClientBook createClientBook(Client client, Book book) {
+        if (client == null || book == null){
+            throw new IllegalArgumentException("there should be no null values");
         }
+        return new ClientBook.Builder().Client(client).Book(book).build();
+
+    }
 }
--- src/test/java/za/ac/cput/factory/ClientBookFactoryTest.java
+++ src/test/java/za/ac/cput/factory/ClientBookFactoryTest.java
@@ -7,25 +7,5 @@ import za.ac.cput.domain.ClientBook;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClientBookFactoryTest {
-    private ClientBook clientBook;
 
-    @BeforeEach
-    void setUp() {
-         clientBook = ClientBookFactory.createClientBook("1","A1");
-    }
-
-    @Test
-    void createClientBook() {
-
-        assertAll(
-                () -> assertNotNull(clientBook),
-                () -> assertSame("1",clientBook.getClientId()),
-                () -> assertSame("A1",clientBook.getBookId()),
-                () -> assertNotSame("2",clientBook.getClientId()),
-                () -> assertNotSame("aa",clientBook.getBookId())
-
-        );
-
-        System.out.println("client book created");
-    }
 }
