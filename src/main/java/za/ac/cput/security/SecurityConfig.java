dd4f8ac4a523d38c8bf83ead34eb1862926e3838
Ziyaad <Zpetersen@Bainscorp.com>
2022-09-25 22:47:36 +0200

    updated security roles

--- src/main/java/za/ac/cput/security/SecurityConfig.java
+++ src/main/java/za/ac/cput/security/SecurityConfig.java
@@ -66,75 +66,80 @@ public class SecurityConfig  {
                 .authorizeRequests()
 
                 //book authentication
-                .antMatchers(HttpMethod.POST,"/books/save_book").hasRole("USER")
-                .antMatchers(HttpMethod.PUT,"/books/update_book").hasRole("USER")
+                .antMatchers(HttpMethod.POST,"/books/save_book").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT,"/books/update_book").hasRole("ADMIN")
                 .antMatchers(HttpMethod.DELETE,"/books/deleteBook/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/books/getAllBooks").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/books/findByName/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/books/findByAuthor/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/books/findByBookId/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/books/getAllBooks").hasRole("USER")
+                .antMatchers(HttpMethod.GET,"/books/findByName/").hasRole("USER")
+                .antMatchers(HttpMethod.GET,"/books/findByAuthor/").hasRole("USER")
+                .antMatchers(HttpMethod.GET,"/books/findByBookId/").hasRole("USER")
 
                 //city authentication
-                .antMatchers(HttpMethod.POST,"/cityController/save_City").hasRole("USER")
-                .antMatchers(HttpMethod.GET,"/cityController/readCity/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST,"/cityController/update_City").hasRole("USER")
+                .antMatchers(HttpMethod.POST,"/cityController/save_City").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/cityController/readCity/").hasRole("USER")
+                .antMatchers(HttpMethod.POST,"/cityController/update_City").hasRole("ADMIN")
                 .antMatchers(HttpMethod.DELETE,"/cityController/deleteCity/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/cityController/getall").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/cityController/getCitiesById").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/cityController/getall").hasRole("USER")
+                .antMatchers(HttpMethod.GET,"/cityController/getCitiesById").hasRole("USER")
 
                 //client address authentication
                 .antMatchers(HttpMethod.POST,"/clientAddress/save_ClientAddress").hasRole("USER")
-                .antMatchers(HttpMethod.GET,"/clientAddress/readClientAddress/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/clientAddress/readClientAddress/").hasRole("USER")
                 .antMatchers(HttpMethod.PUT,"/clientAddress/update_ClientAddress").hasRole("USER")
-                .antMatchers(HttpMethod.DELETE,"/clientAddress/deleteClientAddress/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE,"/clientAddress/deleteClientAddress/").hasRole("ADMIN")//should be able to delete their own Address
                 .antMatchers(HttpMethod.GET,"/clientAddress/getAll_ClientAddresses").hasRole("ADMIN")
 
                 //client book authentication
-                .antMatchers(HttpMethod.POST,"/clientBook/save_clientbook").hasRole("USER")
+                .antMatchers(HttpMethod.POST,"/clientBook/save_clientbook").hasRole("ADMIN")
                 .antMatchers(HttpMethod.GET,"/clientBook/read_clientBook/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT,"/clientBook/update_clientbook").hasRole("USER")
+                .antMatchers(HttpMethod.PUT,"/clientBook/update_clientbook").hasRole("ADMIN")
                 .antMatchers(HttpMethod.DELETE,"/clientBook/delete_clientbook/").hasRole("ADMIN")
                 .antMatchers(HttpMethod.GET,"/clientBook/getAll_clientbooks").hasRole("ADMIN")
+                //TODO: get clientbookByClientId
 
                 //client contact authentication
                 .antMatchers(HttpMethod.POST,"/clientContact/save_ClientContact").hasRole("USER")
                 .antMatchers(HttpMethod.GET,"/clientContact/readClientContact/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT,"/clientContact/update_ClientContact").hasRole("USER")
-                .antMatchers(HttpMethod.DELETE,"/clientContact/deleteClientContact/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT,"/clientContact/update_ClientContact").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE,"/clientContact/deleteClientContact/").hasRole("USER") //should be able to delete their own Contact
                 .antMatchers(HttpMethod.GET,"/clientContact/getAll_ClientContacts").hasRole("ADMIN")
+                //TODO: get clientContact by ClientId
 
                 //client authentication
                 .antMatchers(HttpMethod.POST,"/client/save_client").hasRole("USER")
                 .antMatchers(HttpMethod.GET,"/client/readClient/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT,"/client/update_client").hasRole("USER")
-                .antMatchers(HttpMethod.DELETE,"/client/deleteClient/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT,"/client/update_client").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE,"/client/deleteClient/").hasRole("ADMIN") //should be able to delete their own account
                 .antMatchers(HttpMethod.GET,"/client/getAll_clients").hasRole("ADMIN")
                 .antMatchers(HttpMethod.GET,"/client/find_ClientBy_Id/").hasRole("ADMIN")
+                //TODO: update client by client id + read client by id
 
                 //client gender authentication
-                .antMatchers(HttpMethod.GET,"/clientgender/getAllClientGender").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST,"/clientgender/save_clientGender").hasRole("USER")
-                .antMatchers(HttpMethod.PUT,"/clientgender/update_clientGender").hasRole("USER")
+                .antMatchers(HttpMethod.GET,"/clientgender/getAllClientGender").hasRole("USER")
+                .antMatchers(HttpMethod.POST,"/clientgender/save_clientGender").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT,"/clientgender/update_clientGender").hasRole("ADMIN")
                 .antMatchers(HttpMethod.DELETE,"/clientgender/deleteClientGender/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/clientgender/findClientGenderByClient/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/clientgender/findClientGenderByClient/").hasRole("USER") //should be able to view their own gender
+                //TODO: update Client gender by id?
 
                 //contact authentication
                 .antMatchers(HttpMethod.POST,"/contact/save_contact").hasRole("USER")
                 .antMatchers(HttpMethod.PUT,"/contact/update_contact").hasRole("USER")
-                .antMatchers(HttpMethod.DELETE,"/contact/deleteContact/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE,"/contact/deleteContact/").hasRole("USER") //unsure about this, does this delete user specific contact?
                 .antMatchers(HttpMethod.GET,"/contact/getAllContacts").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/contact/findByCell/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/contact/findByEmail/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/contact/findByContactId/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/contact/findByCell/").hasRole("USER")  //input the users logged in id UNSURE
+                .antMatchers(HttpMethod.GET,"/contact/findByEmail/").hasRole("USER") //input the users logged in id UNSURE
+                .antMatchers(HttpMethod.GET,"/contact/findByContactId/").hasRole("USER") //input the users logged in id UNSURE
 
                 //gender authentication
-                .antMatchers(HttpMethod.GET,"/genders/getAllGenders").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST,"/genders/save_gender").hasRole("USER")
-                .antMatchers(HttpMethod.PUT,"/genders/update_gender").hasRole("USER")
+                .antMatchers(HttpMethod.GET,"/genders/getAllGenders").hasRole("USER")
+                .antMatchers(HttpMethod.POST,"/genders/save_gender").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT,"/genders/update_gender").hasRole("ADMIN")
                 .antMatchers(HttpMethod.DELETE,"/genders/deleteGender/").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET,"/gender/getGenderByGenderId/").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/gender/getGenderByGenderId/").hasRole("USER") //UNSURE
 
                 //librarian address authentication
+                //TODO: Maybe librarian role?
                 .antMatchers(HttpMethod.POST,"/libraryAddress/save_LibrarianAddress").hasRole("USER")
                 .antMatchers(HttpMethod.GET,"/libraryAddress/readLibrarianAddress/").hasRole("ADMIN")
                 .antMatchers(HttpMethod.PUT,"/libraryAddress/update_LibrarianAddress").hasRole("USER")
