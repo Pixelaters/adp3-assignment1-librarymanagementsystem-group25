a14b288dc27ca9d7adf90386aefcf33d525007ca
Ziyaad <Zpetersen@Bainscorp.com>
2022-10-16 17:55:49 +0200

    client book has autogenerated id

--- src/main/java/za/ac/cput/domain/ClientBook.java
+++ src/main/java/za/ac/cput/domain/ClientBook.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -32,6 +33,8 @@ public class ClientBook implements Serializable {
 //    private String dateOrdered;
 
     @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
     String clientBookId;
 
     @ManyToOne
