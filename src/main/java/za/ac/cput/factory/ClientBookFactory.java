32026a1b818e39c0ae38002378cf72c830e99b5a
breySean <breytonernstzen@gmail.com>
2022-10-26 22:16:42 +0200

    test cases fixed

--- src/main/java/za/ac/cput/factory/ClientBookFactory.java
+++ src/main/java/za/ac/cput/factory/ClientBookFactory.java
@@ -5,12 +5,17 @@ import za.ac.cput.domain.Client;
 import za.ac.cput.domain.ClientBook;
 
 public class ClientBookFactory {
-//    public static ClientBook createClientBook(String clientBookId, String clientId, String bookId) {
-//        if (clientBookId == null || clientId == null || bookId == null){
-//            throw new IllegalArgumentException("there should be no null values");
-//        }
-//        return  ClientBook.builder().clientId(clientId).bookId(bookId).build();
-//
-//    }
-//}
+    public static ClientBook createClientBook(String clientBookId, Client clientId, Book bookId, String dateOrdered) {
+        if (clientBookId == null || clientId == null || bookId == null || dateOrdered == null){
+            throw new IllegalArgumentException("there should be no null values");
+        }
+        return  ClientBook.builder().
+                clientBookId(clientBookId).
+                clients(clientId).
+                books(bookId).
+                dateOrdered(dateOrdered).
+                build();
+
+    }
 }
+
--- src/test/java/za/ac/cput/service/impl/ClientBookImplTest.java
+++ src/test/java/za/ac/cput/service/impl/ClientBookImplTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import za.ac.cput.domain.ClientBook;
 import za.ac.cput.factory.BookFactory;
+import za.ac.cput.factory.ClientBookFactory;
 import za.ac.cput.factory.ClientFactory;
 import za.ac.cput.factory.NameFactory;
 import za.ac.cput.repository.BookIRepository;
@@ -18,103 +19,98 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.Alphanumeric.class)
 class ClientBookImplTest {
-//
-//    @Mock
-//    private ClientBookIRepository clientBookIRepository;
-//
-//    ClientBookImpl clientBookService;
-//
-//    private static ClientBook clientBook,updateClientBook;
-//
-//    @BeforeEach
-//    void setUp(){
-//        clientBookService = new ClientBookImpl(clientBookIRepository);
-//
-//        clientBook = new ClientBook.Builder()
-//              .Client(ClientFactory.createClient("1", NameFactory.createName("Breyton","Sean","Ernstzen")))
-//                      .Book (BookFactory.CreateBook("ZZ1","Finding Gobby","James Frank","fiction","description", "false", "https://something.jpg"))
-//                .build();
-//
-//        updateClientBook = new ClientBook.Builder()
-//                .Client(ClientFactory.createClient("1", NameFactory.createName("Breyton","Sean","Ernstzen")))
-//                .Book (BookFactory.CreateBook("ZZ1","Finding Gobby","James Frank","fiction","description", "false", "https://something.jpg"))
-//                .build();
-//    }
-//
-//    @Test
-//    void a_create(){
-//        clientBookService.create(clientBook);
-//
-//        assertAll(
-//                () -> assertNotNull(clientBook),
-//                () -> assertSame("1",clientBook.getClient().getClientId()),
-//                () -> assertSame("ZZ1",clientBook.getBook().getBookId())
-//        );
-//
-//        System.out.println("Client book saved!");
-//        System.out.println();
-//    }
-//
-//    @Test
-//    void b_read(){
-//        clientBookService.read(clientBook.getClient().getClientId());
-//
-//        assertAll(
-//                () -> assertNotNull(clientBook)
-//        );
-//
-//        System.out.println(clientBook);
-//        System.out.println();
-//    }
-//
-//    @Test
-//    void c_update(){
-//        clientBookService.update(updateClientBook);
-//
-//        assertAll(
-//                () -> assertNotSame(clientBook.getBook().getBookId(),updateClientBook.getBook().getBookId()),
-//                () -> assertNotSame(clientBook.getBook().getBookName(),updateClientBook.getBook().getBookName()),
-//                () -> assertNotSame(clientBook.getBook().getAuthor(),updateClientBook.getBook().getAuthor()),
-//                () -> assertNotSame(clientBook.getBook().getGenre(),updateClientBook.getBook().getGenre())
-//        );
-//
-//        System.out.println("Client book details updated");
-//        System.out.printf(updateClientBook.toString());
-//        System.out.println();
-//    }
-//
-//    @Test
-//    void d_delete(){
-//        clientBookService.delete(updateClientBook.getClient().getClientId());
-//
-//        assertAll(
-//                () -> assertNotNull(updateClientBook)
-//        );
-//
-//        System.out.println("Client book deleted");
-//        System.out.println();
-//    }
-//
-//    @Test
-//    void e_getAll(){
-//        clientBookService.getAll();
-//
-//        assertAll(
-//                () -> assertNotNull(clientBookService.getAll())
-//        );
-//
-//        System.out.println(clientBookService.getAll());
-//        System.out.println();
-//    }
-//
-//    @Test
-//    void f_findClientBooksByClientId(){
-//
-//        assertAll(
-//                () -> assertNotNull(clientBook)
-//        );
-//
-//        System.out.println(clientBookService.findClientBookByClient(updateClientBook.getClient()));
-//
-//    }
+
+    @Mock
+    private ClientBookIRepository clientBookIRepository;
+
+    ClientBookImpl clientBookService;
+
+    private static ClientBook clientBook,updateClientBook;
+
+    @BeforeEach
+    void setUp(){
+        clientBookService = new ClientBookImpl(clientBookIRepository);
+
+        clientBook = ClientBookFactory.createClientBook("123456789",ClientFactory.createClient("1010101010",
+                        NameFactory.createName("Dennis","","Zakaria")),
+                BookFactory.CreateBook("123","Finding Gobbie","Anne James","Dixon Rodrigues",
+                        "John Smith","adventure","science","fiction","funf for kids",
+                        "yes","https://myprofile.com"),"2022/10/26");
+
+        updateClientBook = ClientBookFactory.createClientBook("246810322",ClientFactory.createClient("1010101010",
+                        NameFactory.createName("Dennis","Edward","Zakaria")),
+                BookFactory.CreateBook("123","Finding Gobbie","Anne James","Dixon Rodrigues",
+                        "John Smith","adventure","science","fiction","funf for kids",
+                        "yes","https://myprofile.com"),"2022/10/26");
+    }
+
+    @Test
+    void a_create(){
+        clientBookService.create(clientBook);
+
+        assertSame("123456789",clientBook.getClientBookId());
+        assertSame("1010101010",clientBook.getClients().getClientId());
+        assertSame("123",clientBook.getBooks().getBookId());
+        assertNotNull(clientBook);
+
+    }
+
+    @Test
+    void b_read(){
+        clientBookService.read(clientBook.getClientBookId());
+
+        assertAll(
+                () -> assertNotNull(clientBook)
+        );
+
+        System.out.println(clientBook);
+        System.out.println();
+    }
+
+    @Test
+    void c_update(){
+        clientBookService.update(updateClientBook);
+
+        assertAll(
+                () -> assertNotSame(clientBook.getClientBookId(),updateClientBook.getClientBookId()),
+                () -> assertNotSame("54646546",clientBook.getClients().getClientId()),
+                () -> assertSame(clientBook.getClients().getClientId(),updateClientBook.getClients().getClientId())
+        );
+
+        System.out.println("Client book details updated");
+        System.out.printf(updateClientBook.toString());
+        System.out.println();
+    }
+
+    @Test
+    void d_delete(){
+        clientBookService.delete(updateClientBook.getClientBookId());
+
+        assertAll(
+                () -> assertNotNull(updateClientBook)
+        );
+
+        System.out.println("Client book deleted");
+        System.out.println();
+    }
+
+    @Test
+    void e_getAll(){
+        clientBookService.getAll();
+
+        assertAll(
+                () -> assertNotNull(clientBookService.getAll())
+        );
+
+        System.out.println(clientBookService.getAll());
+        System.out.println();
+    }
+
+    @Test
+    void f_findClientBooksByClientId(){
+
+        assertAll(
+                () -> assertNotNull(clientBook)
+        );
+    }
 }
